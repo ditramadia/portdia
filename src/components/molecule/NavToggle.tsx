97829bc9bd57219748adb84107587a6657478fbe
ditramadia <ditrarizqaamadia@gmail.com>
2024-04-13 02:35:59 +0700

    style: fix navbar toggle padding to wider

--- src/components/molecule/NavToggle.tsx
+++ src/components/molecule/NavToggle.tsx
@@ -19,11 +19,11 @@ function NavToggle(props: NavToggleProps) {
   }
 
   return (
-    <div className="relative w-fit mt-1 flex gap-1 glass-blur px-1 py-1 rounded-full">
+    <div className="relative w-fit mt-1 flex gap-1 glass-blur px-2 py-2 rounded-full">
       <div className={`absolute -top-1 left-9 w-8 h-1 bg-white rounded-full transition-transform ease-in-out duration-500 ${activeState === 'about' ? 'translate-x-[100px]' : 'translate-x-0'}`}>
         <div className="absolute -inset-2 bg-tertiary rounded-full blur-xl" />
       </div>
-      <div className={`absolute top-1 left-1 w-24 h-10 bg-white/10 rounded-full transition-transform ease-in-out duration-500 ${activeState === 'about' ? 'translate-x-[100px]' : 'translate-x-0'}`} />
+      <div className={`absolute top-2 left-2 w-24 h-10 bg-white/10 rounded-full transition-transform ease-in-out duration-500 ${activeState === 'about' ? 'translate-x-[100px]' : 'translate-x-0'}`} />
       <div 
         className={`w-24 bg-gradient-to-r ${activeState === 'work' ? '' : 'hover:from-white/10 hover:to-transparent'} px-2 py-2 rounded-full flex items-center justify-center body text-on-surface cursor-pointer`}
         onClick={() => handleNavigate('work')}
