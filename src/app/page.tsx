eb7b08d47f39a48ed77fd67d2a9fad7ce4190005
ditramadia <ditrarizqaamadia@gmail.com>
2024-04-14 00:38:26 +0700

    feat: add sticky scroll animation for project hero section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,24 +1,44 @@
 "use client";
 
+import { useRef } from "react";
+import { motion, useMotionValueEvent, useScroll } from "framer-motion";
+
 import Navbar from "@/components/organism/Navbar";
 
 export default function Home() {
+  const planet = useRef(null);
+  const { scrollYProgress } = useScroll({
+    target: planet
+  });
+
+  useMotionValueEvent(scrollYProgress, "change", (latest) => {
+    console.log(latest);
+  });
+
   return (
     <>
       <main>
         <div className="relative">
-          <div className="relative w-11/12 max-h-[700px] h-screen mx-auto pt-32 pb-14 flex flex-col justify-center">
-            <div className="mb-20">
-              <h1 className="heading-1 text-center tracking-[0.25em] leading-tight text-on-surface">Explore my Projects</h1>
-              <h5 className="mt-10 display-5 text-center text-on-surface">
-                I am a full stack developer specialized in <br />
-                <span className="text-tertiary abril-fatface italic">Front-End</span> and <span className="text-tertiary abril-fatface italic">Motion</span>
-              </h5>
-            </div>
+          <div className="relative w-11/12 h-[200vh] mx-auto md:h-[250vh] ">
+            <div className="sticky top-0 w-full h-screen pt-32 pb-14 flex flex-col justify-center">
+              <div className="mb-20">
+                <motion.h1 
+                  className="heading-1 text-center tracking-[0.25em] leading-tight text-on-surface"
+                >
+                  Explore my Projects
+                </motion.h1>
+                <motion.h5
+                  className="mt-10 display-5 text-center text-on-surface"
+                >
+                  I am a full stack developer specialized in <br />
+                  <span className="text-tertiary abril-fatface italic">Front-End</span> and <span className="text-tertiary abril-fatface italic">Motion</span>
+                </motion.h5>
+              </div>
 
-            <div className="absolute bottom-5 inset-x-0 flex flex-col items-center md:gap-2">
-              <div className="h-10 w-[1px] bg-on-surface" />
-              <p className="body text-on-surface">Scroll</p>
+              <div className="absolute bottom-5 inset-x-0 flex flex-col items-center md:gap-2">
+                <div className="h-10 w-[1px] bg-on-surface" />
+                <p className="body text-on-surface">Scroll</p>
+              </div>
             </div>
           </div>
 
@@ -29,6 +49,29 @@ export default function Home() {
             </div>
           </div>
         </div>
+
+        <h1 className="heading-1 text-surface">Rest of the page</h1>
+        <h1 className="heading-1 text-surface">Rest of the page</h1>
+        <h1 className="heading-1 text-surface">Rest of the page</h1>
+        <h1 className="heading-1 text-surface">Rest of the page</h1>
+        <h1 className="heading-1 text-surface">Rest of the page</h1>
+        <h1 className="heading-1 text-surface">Rest of the page</h1>
+        <h1 className="heading-1 text-surface">Rest of the page</h1>
+        <h1 className="heading-1 text-surface">Rest of the page</h1>
+        <h1 className="heading-1 text-surface">Rest of the page</h1>
+        <h1 className="heading-1 text-surface">Rest of the page</h1>
+        <h1 className="heading-1 text-surface">Rest of the page</h1>
+        <h1 className="heading-1 text-surface">Rest of the page</h1>
+        <h1 className="heading-1 text-surface">Rest of the page</h1>
+        <h1 className="heading-1 text-surface">Rest of the page</h1>
+        <h1 className="heading-1 text-surface">Rest of the page</h1>
+        <h1 className="heading-1 text-surface">Rest of the page</h1>
+        <h1 className="heading-1 text-surface">Rest of the page</h1>
+        <h1 className="heading-1 text-surface">Rest of the page</h1>
+        <h1 className="heading-1 text-surface">Rest of the page</h1>
+        <h1 className="heading-1 text-surface">Rest of the page</h1>
+        <h1 className="heading-1 text-surface">Rest of the page</h1>
+        <h1 className="heading-1 text-surface">Rest of the page</h1>
 
         <Navbar active="work"/>
         
