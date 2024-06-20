e4d213f187f8d2e14d6968f1068caf8692d6c1cc
ditramadia <ditrarizqaamadia@gmail.com>
2024-06-20 18:51:58 +0700

    feat: add animation to product section

--- src/components/section/ProductSection.tsx
+++ src/components/section/ProductSection.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { motion } from 'framer-motion';
 
 import DotHeader from '@/components/display/DotHeader';
 import ProductCard from '@/components/card/ProductCard';
@@ -10,10 +11,18 @@ const ProductSection = () => {
     <div className='container-sm mt-36 flex flex-col items-center md:container-base'>
       <DotHeader alignment='center' title='PRODUCTS' subtitle='What I can give you.'/>
 
-      <div className='mt-16 flex flex-col gap-12 items-center md:flex-row md:mt-20'>
+      <div className='mt-16 flex flex-col gap-12 items-center md:flex-row md:mt-20 md:gap-6'>
         {
           products.map((product) => (
-            <ProductCard key={product.id} title={product.title} description={product.description} image={product.image} />
+            <motion.div 
+              key={product.id}
+              initial={{ opacity: 0, y: 24 }}
+              whileInView={{ opacity: 1, y: 0 }}
+              transition={{ duration: 0.5, delay: 0.1 * product.id, ease: 'easeInOut' }}
+              viewport={{ once: true }}
+            >
+              <ProductCard title={product.title} description={product.description} image={product.image} />
+            </motion.div>
           ))
         }
       </div>
