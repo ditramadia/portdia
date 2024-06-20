40e8c89d97dd50f9b6f54f1e5ce720886cf65d1d
ditramadia <ditrarizqaamadia@gmail.com>
2024-06-20 18:27:14 +0700

    feat: add dotted header

---
+++ src/components/section/ProductSection.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+import DotHeader from '@/components/display/DotHeader'
+
+const ProductSection = () => {
+  return (
+    <div>
+      <DotHeader alignment='center' title='PRODUCTS' subtitle='What I can give you.'/>
+    </div>
+  );
+};
+
+export default ProductSection;
